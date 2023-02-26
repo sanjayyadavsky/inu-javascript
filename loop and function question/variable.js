69757fc3c3b547a47e1fa4c27990ccc046bafbdb
sanjayyadavsky <sanjayyadavsky57@gmail.com>
2023-02-27 00:49:20 +1100

    given question

--- loop and function question/variable.js
+++ loop and function question/variable.js
@@ -1,1 +1,49 @@
-var x = sanjay 
+// 1. Create a variable.js file and declare variables and assign string, boolean, undefined and null data types, Display all the value with there type.
+
+// var x = "sanjay " ;
+// console.log(typeof x)
+
+// var x = true 
+// console.log(typeof x)
+
+// var x 
+// console.log(typeof x)
+
+// var x = 
+// console.log(typeof x);
+
+
+// 2. Declare variables to store your first name, last name, marital status, country and age and display them using interploation method.
+
+// let first_Name = "sanjay";
+// let las_tNamae = "Yadav";
+// let marital_mtatus = "Married";
+// let country = "Nepal";
+// let age = "23";
+
+// console.log(`hay my name is ${first_Name} ${las_tNamae} and i am ${age} years old and from ${country}, and i am ${marital_mtatus} too`);
+
+// 3. Decalre a varibale and assign value to it and change all the string characters to capital letters using toUpperCase() method.
+
+// let Name = "Sanjay Yadav"
+// console.log(Name.toLocaleUpperCase());
+
+// 4. Check if the string contains a word Script using includes() method.
+// let Typeof = "Script, Boolen";
+// console.log(Typeof.includes("Script")); (method;- arry.method i.e inscludes())
+
+// 5. Split the string into an array using split() method
+// let x  = "the quick brown fox jump over the lazy dog"
+// console.log(x.split( " "))
+
+// 6. 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+// let x = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// let newarr = x.split( ", " )
+// let Arry = [];
+// Arry.push(x)
+//  console.log(typeof Arry)
+// console.log(Arry);
+
+// var companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+// var companyArray = companies.split(', ');
+// console.log(companyArray);
